8623ee7e356ec8c18e3a98f619e2a0f5a7d403b9
Alex Schmith <alexanderschmith@protonmail.com>
2024-09-11 14:28:53 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd /srv/solardebris.github.io/backend/
-gunicorn -w 4 -b 0.0.0.0:5000 blog:app
+cd /app/backend/
+gunicorn -w 4 -b 0.0.0.0:5000 blog:app & 
 
-cd /srv/solardebris.github.io/frontend
-npm run build 
+cd /app/frontend
 npm run start
 
